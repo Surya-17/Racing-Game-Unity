29c80b96c62cabb6179f6d686768467f9e13acd1
Surya-17 <suryakartheekb@gmail.com>
2022-01-20 15:42:00 +0530

    file upload

---
+++ Assets/Scripts/EndScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScreen : MonoBehaviour
+{
+    public GameObject EndScrn;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+
+            EndScrn.gameObject.SetActive(true);
+         }
+    }
+
+    private void Awake()
+    {
+        EndScrn.gameObject.SetActive(false);
+    }   
+}
